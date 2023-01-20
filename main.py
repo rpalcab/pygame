2adbacdb9b81de1511c66fe44a3504370c58d8d0
rpalcab <59095005+rpalcab@users.noreply.github.com>
2023-01-20 19:51:36 +0000

    Up to collisions

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ text_bg = test_font.render('My game', True, 'Black')    # Text to image. Do NOT 
 
 # Characters
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()  # Convert_alpha to respect transparent bg
-snail_x_pos = 800                               # Defines initial snail position
+snail_rect = snail_surface.get_rect(bottomleft = (800, 300))
 
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80, 300))          # Creates player rectangle for collisions etc. Topleft indicates reference of rectangle (corner topleft in this case)
@@ -24,13 +24,27 @@ while True:                                     # Avoids game from exiting, infi
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()                          # Cleanly quits
-    
+        # IDEA: Crush snails if you press them (any mouse button)
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('OUCH')
+        #         print(pygame.mouse.get_pressed())
+        #     if snail_rect.collidepoint(event.pos):
+        #         print('PRFT...')
+        #         print(pygame.mouse.get_pressed())
+        
+
     screen.blit(sky_bg, (0,0))                  # Sets background images
     screen.blit(ground_bg, (0,300))
     screen.blit(text_bg, (10,10))
-    snail_x_pos -= 4                            # Changes x position with each loop
-    if snail_x_pos < -100 : snail_x_pos = 800   # Back to the left
-    screen.blit(snail_surface, (snail_x_pos, 265))
+
+    snail_rect.x -= 4                           # Moves snail to the left
+    if snail_rect.right <= 0: snail_rect.left = 800     # Snail goes back to the right
+    screen.blit(snail_surface, snail_rect)      # Places snail
     screen.blit(player_surface, player_rect)    # Places player image and rectangle
+    
+#    if player_rect.colliderect(snail_rect):
+#        pass
+
     pygame.display.update()
     clock.tick(60)                              # Max of 60 times per second, advances in time
